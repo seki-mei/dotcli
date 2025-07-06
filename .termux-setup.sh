22cadb6a5be448788877cdbc28482432209cddaf
seki-mei <>
2025-07-07 00:07:40 +0200

    added termux setup scripts

---
+++ .termux-setup.sh
@@ -0,0 +1,18 @@
+mv dotfiles/.* .
+rmdir dotfiles
+
+pkg install termux-auth \
+	termux-api \
+	vim \
+	zsh \
+	git \
+	rclone \
+	fzf \
+	bat \
+	tree \
+	fd \
+	wget
+
+mkdir -p $HOME/.termux
+wget https://raw.githubusercontent.com/termux/termux-styling/refs/heads/master/app/src/main/assets/colors/gruvbox-dark.properties
+mv gruvbox-dark.properties .termux/colors.properties
