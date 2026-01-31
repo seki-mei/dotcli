eb53729909fd59993fb060981b0bb2de7ac822b5
seki-mei <>
2026-01-31 13:37:25 +0100

    termux packages

--- .termux-setup.sh
+++ .termux-setup.sh
@@ -4,16 +4,18 @@ rmdir dotcli
 pkg install termux-auth \
 	termux-api \
 	termux-tools \
-	nvim \
-	zsh \
-	git \
-	rclone \
-	fzf \
 	bat \
-	tree \
 	fd \
 	figlet \
+	fzf \
+	git \
+	htop \
+	nvim \
+	rclone \
+	tree \
+	unzip
 	wget
+	zsh \
 
 mkdir -p $HOME/.termux
 wget https://raw.githubusercontent.com/termux/termux-styling/refs/heads/master/app/src/main/assets/colors/gruvbox-dark.properties
